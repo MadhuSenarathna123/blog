d8078730ce494f28c49857727b57f140a9156f38
dmadhu123 <dulikamadushani1234@gmail.com>
2021-07-07 16:25:48 +0530

    created settings page UI

---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,39 @@
+import "./settings.css"
+import Sidebar from "../../components/sidebar/Sidebar";
+
+export default function Settings(){
+    return (
+        <div className="settings">
+            <div className="settingsWrapper">
+                <div className="settingsTitle">
+                   <span className="settingsUpdateTitle">Update Your Account</span>
+                   <span className="settingsDeleteTitle">Delete Account</span> 
+                </div>
+                <form className="settingsForm">
+                    <label>Profile Picture</label>
+                    <div className="settingsPP">
+                        <img 
+                        src="https://images.statusfacebook.com/profile_pictures/nice/nice_profile_pictures_10.jpg"
+                        alt=""
+                        />
+                        <label htmlFor="fileInput">
+                        <i className="settingsPPIcon fas fa-user-circle"></i>
+                        </label>
+                        <input type="file" id="fileInput" style={{display:"none"}}/>
+                    </div>
+
+                    <label>User Name</label>
+                    <input type="text" placeholder="Madhushani"/>
+                    <label>Email</label>
+                    <input type="email" placeholder="madushani@gmail.com"/>
+                    <label>Password</label>
+                    <input type="Password"/>
+
+                    <button className="settingsSubmit">Update</button>
+
+                </form>
+            </div>
+            <Sidebar/>
+        </div>
+    )
+}
